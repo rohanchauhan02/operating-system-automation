c1fc2ffe50fa9b61f3a82c3235c5a5f2de536dcc
rohanchauhan02 <rohan021195@gmail.com>
2021-01-14 10:30:23 +0530

    init commit

---
+++ location.js
@@ -0,0 +1,8 @@
+var id=setInterval(showMouseLocation,100);
+function showMouseLocation(){
+    var mouse=robot.getMousePos();
+    console.log(mouse);
+    if(mouse.x==0 && mouse.y==0){
+        clearInterval(id);
+    }
+}
