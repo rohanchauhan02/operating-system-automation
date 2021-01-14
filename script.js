71b3c5c8f28d79c02e97f5d404ec03fed398f56c
rohanchauhan02 <rohan021195@gmail.com>
2021-01-14 10:37:59 +0530

    commit

--- script.js
+++ script.js
@@ -1,22 +1,50 @@
 var robot=require("robotjs");
 var screen=robot.getScreenSize();
 console.log(screen);
-setTimeout(openCalculator,0);
-function openCalculator(){
+setTimeout(handleOpenboard,0);
+function handleOpenboard(){
     robot.moveMouseSmooth(64,864);
     // console.log(robot.getMousePos())
     robot.mouseClick();
-    robot.typeString(" calculator");
+    robot.typeString('Openboard');
     robot.keyTap("enter");
+    setTimeout(writeHi,2000);
+}
+function writeHi(){
+    robot.moveMouseSmooth(400,250);
+    robot.mouseToggle("down","left")
+    robot.dragMouse(400,450);
+    robot.mouseToggle("up","left");
 
+    robot.dragMouse(400,350);
+    robot.mouseToggle("down","left");
+    robot.dragMouse(600,350);
+    robot.mouseToggle("up","left");
+
+    robot.moveMouseSmooth(600,250);
+    robot.mouseToggle("down","left")
+    robot.dragMouse(600,450);
+    robot.mouseToggle("up","left");
+
+    robot.moveMouseSmooth(700,250);
+    robot.mouseToggle("down","left")
+    robot.dragMouse(700,450);
+    robot.mouseToggle("up","left");
+
+    robot.moveMouseSmooth(1440,20);
+    robot.mouseClick();
+    setTimeout(openWorld,2000);
 
 }
+function openWorld(){
+    robot.moveMouseSmooth(64,864);
+    // robot.mouseClick();
+    // robot.typeString('World');
+    // robot.keyTap("enter");
+    setTimeout(function(){
+        robot.mouseClick();
+        robot.typeString(' World');
+        robot.keyTap("enter");
+    },2000);
 
-// var id=setInterval(showMouseLocation,100);
-// function showMouseLocation(){
-//     var mouse=robot.getMousePos();
-//     console.log(mouse);
-//     if(mouse.x==0 && mouse.y==0){
-//         clearInterval(id);
-//     }
-// }
+}
